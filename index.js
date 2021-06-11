a4f0743bb1d7f2e33a46108f72b5471060089452
DorkyTim <dorkytim@gmail.com>
2021-06-11 07:57:21 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,21 @@
+const Discord = require('discord.js');
+const { prefix, token } = require('./config.json');
+const client = new Discord.Client();
+
+client.once('ready', () => {
+	console.log('Ready!');
+});
+
+client.login(token);
+
+client.on('message', message => {
+	if (message.content === `${prefix}ping`) {
+		message.channel.send('Pong.');
+	}
+	else if (message.content === `${prefix}beep`) {
+		message.channel.send('Boop.');
+	}
+	else if (message.content === `${prefix}server`) {
+		message.channel.send(`Server name: ${message.guild.name}\nTotal members: ${message.guild.memberCount}`);
+	}
+});
